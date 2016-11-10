c8ce4dd576078795d28ea74fa108b7127ba99995
huyaming <441207266@qq.com>
2016-11-10 21:59:28 +0800

    查看大图

--- Geely_Third/C/mediaPlayer/GeelyPictureShowVC.m
+++ Geely_Third/C/mediaPlayer/GeelyPictureShowVC.m
@@ -14,6 +14,7 @@
     UIScrollView *scroll;
     UILabel *countLabel;
     NSInteger page;
+    UIView *fullView;
 }
 
 @end
@@ -87,6 +88,11 @@
     [bottomTool addSubview:rightBtn];
     [self.contentScrollView addSubview:bottomTool];
     //
+    UIButton *showFullBtn = [[UIButton alloc] initWithFrame:CGRectMake(bottomTool.frame.size.width-72, 0, 72,62)];
+    [showFullBtn addTarget:self action:@selector(showFullPic:) forControlEvents:UIControlEventTouchUpInside];
+    //showFullBtn.backgroundColor = [UIColor redColor];
+    [bottomTool addSubview:showFullBtn];
+    //
     UIImageView *rightImgs = [[UIImageView alloc] initWithFrame:CGRectMake(scroll.frame.origin.x+scroll.frame.size.width+55, scroll.frame.origin.y, 260, 320)];
     rightImgs.image = [UIImage imageNamed:@"picture_right_imgs_03"];
     [self.contentScrollView addSubview:rightImgs];
@@ -99,6 +105,58 @@
     countLabel.textColor = RGB(171, 171, 171);
     //countLabel.contentMode = UIViewContentModeTop;
     [self.contentScrollView addSubview:countLabel];
+}
+- (void)showFullPic:(UIButton*)sender{
+    fullView = [[UIView alloc] initWithFrame:CGRectMake(0, 0,1310,492)];
+    fullView.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.9];
+    fullView.center = self.view.center;
+    [self.view addSubview:fullView];
+    //
+    UIScrollView *sclView = [[UIScrollView alloc] initWithFrame:CGRectMake(0,0,fullView.frame.size.width,fullView.frame.size.height)];
+    sclView.contentSize = CGSizeMake(sclView.frame.size.width*3,sclView.frame.size.height);
+    sclView.tag=999;
+    sclView.pagingEnabled=YES;
+    sclView.showsVerticalScrollIndicator=NO;
+    sclView.showsHorizontalScrollIndicator=YES;
+    sclView.userInteractionEnabled=NO;
+    sclView.delegate = self;
+    for (NSInteger i=0; i<3; i++) {
+        UIView *bg = [[UIView alloc] initWithFrame:CGRectMake(sclView.frame.size.width*i, 0,sclView.frame.size.width, sclView.frame.size.height)];
+        UIImageView *img = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 874, bg.frame.size.height)];
+        img.center = CGPointMake(bg.frame.size.width*0.5,img.center.y);
+        img.image = [UIImage imageNamed:@"picture_bg"];
+        [bg addSubview:img];
+        [sclView addSubview:bg];
+    }
+    [sclView setContentOffset:CGPointMake(sclView.frame.size.width,sclView.contentOffset.y) animated:NO];
+    [fullView addSubview:sclView];
+    //
+    UILabel *titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(22.5+35, 0, 1310-53, 48)];
+    titleLabel.text = @"午后阳光.png";
+    titleLabel.textColor = [UIColor whiteColor];
+    titleLabel.font = [UIFont systemFontOfSize:16.5];
+    titleLabel.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.2];
+    [fullView addSubview:titleLabel];
+    //
+    UIImageView *bottomBg = [[UIImageView alloc] initWithFrame:CGRectMake(titleLabel.frame.origin.x-35,fullView.frame.size.height-62-10, fullView.frame.size.width-53, 62)];
+    bottomBg.image = [UIImage imageNamed:@"picture_full_bg"];
+    bottomBg.userInteractionEnabled = YES;
+    [fullView addSubview:bottomBg];
+    //
+    UIButton *leftBtn = [[UIButton alloc] initWithFrame:CGRectMake(0,0,72,62)];
+    [leftBtn addTarget:self action:@selector(showPrecedingPic) forControlEvents:UIControlEventTouchUpInside];
+    //leftBtn.backgroundColor = [UIColor redColor];
+    [bottomBg addSubview:leftBtn];
+    //
+    UIButton *rightBtn = [[UIButton alloc] initWithFrame:CGRectMake(110,0,72,62)];
+    [rightBtn addTarget:self action:@selector(showNextPic) forControlEvents:UIControlEventTouchUpInside];
+    //rightBtn.backgroundColor = [UIColor redColor];
+    [bottomBg addSubview:rightBtn];
+    //
+    UIButton *closeBtn = [[UIButton alloc] initWithFrame:CGRectMake(bottomBg.frame.size.width-72, 0, 72,62)];
+    [closeBtn addTarget:self action:@selector(showFullPic) forControlEvents:UIControlEventTouchUpInside];
+    //showFullBtn.backgroundColor = [UIColor redColor];
+    [bottomBg addSubview:closeBtn];
 }
 - (void)showPrecedingPic:(UIButton*)sender{
     if(page>1){
@@ -115,6 +173,20 @@
 }
 - (void)scrollViewDidEndScrollingAnimation:(UIScrollView *)scrollView{
     [scrollView setContentOffset:CGPointMake(scrollView.frame.size.width,scrollView.contentOffset.y) animated:NO];
+}
+#pragma mark - 全屏浏览事件
+- (void)showPrecedingPic{
+    UIScrollView*sclView = [fullView viewWithTag:999];
+    [sclView setContentOffset:CGPointMake(sclView.contentOffset.x-sclView.frame.size.width, sclView.contentOffset.y) animated:YES];
+    
+}
+- (void)showNextPic{
+    UIScrollView*sclView = [fullView viewWithTag:999];
+    [sclView setContentOffset:CGPointMake(sclView.contentOffset.x+sclView.frame.size.width, sclView.contentOffset.y) animated:YES];
+}
+- (void)showFullPic{
+    [fullView removeFromSuperview];
+    fullView=nil;
 }
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
